b03e8a6fa6fce37feeba61b3bfa71b667161b928
Bao1404 <lethebao1404@gmail.com>
2025-10-07 00:38:20 +0700

    modify

--- CashBackService/Services/RequestService.cs
+++ CashBackService/Services/RequestService.cs
@@ -1,4 +1,6 @@
-﻿using CashBackService.Interfaces;
+﻿using CashBackObject.Models;
+using CashBackRepositories.Interfaces;
+using CashBackService.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +11,13 @@ namespace CashBackService.Services
 {
     public class RequestService : IRequestService
     {
-        private readonly CashBackRepositories.Interfaces.IRequestRepository _requestRepository;
-        public RequestService(CashBackRepositories.Interfaces.IRequestRepository requestRepository)
+        private readonly IRequestRepository _requestRepository;
+        public RequestService(IRequestRepository requestRepository)
         {
             _requestRepository = requestRepository;
         }
-        public Task CreateContactRequest(CashBackObject.Models.ContactRequest request) => _requestRepository.CreateContactRequest(request);
-        public Task<List<CashBackObject.Models.ContactRequest>> GetAllContactRequests() => _requestRepository.GetAllContactRequests();
-        public Task UpdateContactRequest(CashBackObject.Models.ContactRequest request) => _requestRepository.UpdateContactRequest(request);
+        public Task CreateContactRequest(ContactRequest request) => _requestRepository.CreateContactRequest(request);
+        public Task<List<ContactRequest>> GetAllContactRequests() => _requestRepository.GetAllContactRequests();
+        public Task UpdateContactRequest(ContactRequest request) => _requestRepository.UpdateContactRequest(request);
     }
 }
